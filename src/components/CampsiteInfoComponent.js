4155e88d19f48c8f4ed3ee12a8ff241991e54e16
Ryan Harley <rharley.dev@gmail.com>
2020-05-30 13:43:20 -0500

    called comments to DirectoryComponent and make CampsiteInfoComponent file

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { CAMPSITES } from '../shared/campsites'
+
+class CampsiteInfo extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            campsite: CAMPSITES
+        }
+    }
+
+    renderCampsite(campsite) {
+        return (
+                <Card className='col-md-5 m-1'>
+                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardBody>
+                        <CardTitle>{campsite.name}</CardTitle>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+        );
+    }
+
+    renderComments(comments) {
+        if(comments) {
+            return (
+                <div className='col-md-5 m-1'>
+                    <h4>Comments</h4>
+                    <div>
+                        {comments.map(comment => 
+                            <div key={comment.id}>
+                                {comment.text}
+                                <div key={comment.id}>{comment.author} -- {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</div>
+                                <br></br>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            );
+        }
+        return <div />
+    }
+
+    render() {
+        if (this.props.campsite) {
+            return (
+                <div className='row'>{this.renderCampsite(this.props.campsite)} {this.renderComments(this.props.campsite.comments)}</div>
+            );
+        }
+        return <div />;
+    }
+}
+
+export default CampsiteInfo
